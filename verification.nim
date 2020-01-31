7c46796edd93d9dcb17b173fe4be99189673a101
Bartosz Białas <belamenso@gmail.com>
2020-01-31 20:54:16 +0100

    INTERNAL ERROR OF THE COMPILER - verification.nim

--- verification.nim
+++ verification.nim
@@ -1,7 +1,7 @@
 import
   sugar, sequtils
 import
-  sudoku, examples
+  sudoku, examples, types
 
 type
   InvalidityKind* = enum
@@ -23,13 +23,11 @@ proc newRepeatedValue(idx1, idx2: I99): Invalidity =
     sameCol: idx1.y == idx2.y,
     sameBlock: idx1.I99_I33 == idx2.I99_I33)
 
-
 iterator report(seen: var seq[(I99,I99)], idx1, idx2: I99): Invalidity =
   if (idx1, idx2) notin seen:
     seen.add (idx1, idx2)
     seen.add (idx2, idx1)
     yield newRepeatedValue(idx1, idx2)
-
 
 iterator invalidities*(board: Board): Invalidity =
   var seen: seq[(I99,I99)]
@@ -46,36 +44,29 @@ iterator invalidities*(board: Board): Invalidity =
       if idx != idx1 and v == v1:
         for x in report(seen, idx, idx1): yield x
 
+iterator invaliditiesIterator(board: Board, it: iterator(_:Board): (I99, Value) {.closure.}): Invalidity =
+  var seen: seq[(I99,I99)]
+  let mask = fullMask().filter((idx, _) => board[idx] != 0)
+
+  for idx, v in board.it():
+    for idx1, v1 in board.it():
+      if idx != idx1 and v == v1:
+        for x in report(seen, idx, idx1): yield x
 
 iterator invaliditiesInRow*(board: Board, ri: I9): Invalidity =
-  var seen: seq[(I99,I99)]
-  let mask = fullMask().filter((idx, _) => board[idx] != 0)
-
-  for idx, v in board.rowI99(ri, mask):
-    for idx1, v1 in board.rowI99(ri, mask):
-      if idx != idx1 and v == v1:
-        for x in report(seen, idx, idx1): yield x
-
+  for y in invaliditiesIterator(board, iterator(b: Board): (I99, Value) {.closure.} =
+    for z in board.rowI99(ri): yield z):
+    yield y
 
 iterator invaliditiesInColumn*(board: Board, ci: I9): Invalidity =
-  var seen: seq[(I99,I99)]
-  let mask = fullMask().filter((idx, _) => board[idx] != 0)
-
-  for idx, v in board.columnI99(ci, mask):
-    for idx1, v1 in board.columnI99(ci, mask):
-      if idx != idx1 and v == v1:
-        for x in report(seen, idx, idx1): yield x
-      
+  for y in invaliditiesIterator(board, iterator(b: Board): (I99, Value) {.closure.} =
+    for z in board.columnI99(ci): yield z):
+    yield y
 
 iterator invaliditiesInBlock*(board: Board, bi: I33): Invalidity =
-  var seen: seq[(I99,I99)]
-  let mask = fullMask().filter((idx, _) => board[idx] != 0)
-
-  for idx, v in board.blockI99(bi, mask):
-    for idx1, v1 in board.blockI99(bi, mask):
-      if idx != idx1 and v == v1:
-        for x in report(seen, idx, idx1): yield x
-
+  for y in invaliditiesIterator(board, iterator(b: Board): (I99, Value) {.closure.} =
+    for z in board.blockI99(bi): yield z):
+    yield y
 
 proc valid*(board: Board): bool =
   for _ in invalidities(board):
@@ -98,5 +89,8 @@ proc validBlock*(board: Board, bi: I33): bool =
   true
 
 when isMainModule:
-  for x in invalidities(harderBoard):
+  var b = harderBoard
+  b[0] = 5.Value
+  b[2] = 5.Value
+  for x in invaliditiesInRow(b, 0):
     echo x
